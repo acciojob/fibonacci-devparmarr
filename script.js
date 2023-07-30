004689c22b02f2400ae7d964d20e81f39edc97ef
Dev Parmar <89907756+devparmarr@users.noreply.github.com>
2023-07-30 22:51:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function fibonacci(num) {
-// your code here
+  if (num <= 0) return 0; // Base case: Fibonacci of 0 or negative numbers is 0
+  if (num === 1) return 1; // Base case: Fibonacci of 1 is 1
+
+  // Recursive step: Fibonacci of num is the sum of the previous two Fibonacci numbers
+  return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
